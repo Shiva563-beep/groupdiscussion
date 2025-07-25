a8e8fce864a3674e9961dc281f1d42919c91c769
shiva singh <shivanand1801@gmail.com>
2025-07-25 22:47:11 +0530

    conflict case

--- Todu_infra/main.tf
+++ Todu_infra/main.tf
@@ -8,6 +8,11 @@ module "azurerm_resource_group1" {
   resource_group_name = "shiva-rg2"
   location            = "Japan East"
 }
+module "azurerm_resource_group4" {
+  source              = "../Module/azurerm_resource_group"
+  resource_group_name = "shiva-rg5"
+  location            = "Japan East"
+}
 module "virtual_network" {
   depends_on           = [module.azurerm_resource_group]
   source               = "../Module/azurerm_virtual_network"
